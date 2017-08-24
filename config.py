a5f3717dfb1514b168b69bd0730bffe78fe24b6b
sergeylenkovets <sergey.lenkovets@gmail.com>
2017-08-24 11:58:34 +0300

    SQLite

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+#SQLALCHEMY_DATABASE_URI = 'postgresql://sergey:9byajhvth1@localhost:5432/microblog'
+#engine = create_engine('SQLALCHEMY_DATABASE_URI')
+
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'Super_Secret_String'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
